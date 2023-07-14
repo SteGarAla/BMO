62739f808ddfc887bc52034c1df4a0114c2b617e
garala <stegarc@ut.utm.edu>
2023-07-13 22:57:27 -0500

    Login and Sign-up Pages Created

---
+++ src/app/login/page.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="bg-white w-96 rounded-lg shadow-lg p-8">
+        <div className="mb-8">
+          <h2 className="text-2xl font-bold text-center">SIGN-UP</h2>
+        </div>
+        <form className="flex flex-col gap-4">
+        <input
+            type='Name'
+            id="login"
+            className="border border-gray-300 rounded-md p-3"
+            placeholder="Full Name"
+          />
+
+          <input
+            type="email"
+            id="login"
+            className="border border-gray-300 rounded-md p-3"
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            id="password"
+            className="border border-gray-300 rounded-md p-3"
+            placeholder="Password"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded-md mt-4"
+          >
+            Log In
+          </button>
+        </form>
+        <div className="mt-4 text-center">
+          <p className="text-gray-500">Have an account? <Link href="/" className="text-blue-500">Login</Link></p>
+        </div>
+      </div>
+    </div>
+  );
+}
